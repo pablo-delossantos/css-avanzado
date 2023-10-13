a06035aa9ffc1a1c2e9bac6c171fad8979ff3f9c
Pablo de los Santos <estudiodedalo@gmail.com>
2023-10-13 18:28:34 -0300

    Archivos del docente del de curso CSS avanzado

---
+++ XX-Udemy-CSS-avanzado/Animaciones/menu/main.js
@@ -0,0 +1,6 @@
+let enlaces = document.querySelector('.enlaces');
+let menuHamburguesa = document.querySelector('.menu-hamburguesa');
+
+menuHamburguesa.addEventListener('click', function(){
+    enlaces.classList.toggle('enlaces1');
+})
